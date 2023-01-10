75d4129c3ff512da0b9a08a8799d1e240fae9917
Grey0808 <sergeygagarin89629730808@gmail.com>
2023-01-10 23:47:23 +0300

    delete unnecessary

--- app/player.py
+++ app/player.py
@@ -1,27 +1,15 @@
 import app.variables as var
 
-# keyboard
-keyboard1 = var.keyboard1
-keyboard2 = var.keyboard2
 bot = var.bot
 
 
 class Player(object):
-    def __init__(self, id, money=500, gamemode=0, curr_keyboard=0):
+    def __init__(self, id):
         self.id = id
-        self.money = money
-        self.gamemode = gamemode
-        if curr_keyboard == 0:
-            self.curr_keyboard = keyboard1
-        elif curr_keyboard == 1:
-            self.curr_keyboard = keyboard2
 
-    def send(self, string: str):
+        self.money = 500
+        self.gamemode = 0
+        self.curr_keyboard = var.keyboard1
+
+    def send(self, string):
         bot.send_message(self.id, string, reply_markup=self.curr_keyboard)
-
-        if self.money < 10:
-            self.money = 500
-            bot.send_message(self.id, "Боги откатили твой баланс до 500₽", reply_markup=self.curr_keyboard)
-
-    def getinfo(self):
-        return str(self.id) + " " + str(self.money)

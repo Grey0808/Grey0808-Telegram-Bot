adc73c6d9a0810fcf053851c7f642c34350e1b78
Grey0808 <sergeygagarin89629730808@gmail.com>
2023-01-10 20:16:04 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ os.chdir("files")
 with open("key.txt", "w") as file:
     key = input("Введите ключ бота: ")
     file.write(key)
-with open("base.txt", "w") as file:
+with open("base.txt", "wb") as file:
     pickle.dump([], file)
